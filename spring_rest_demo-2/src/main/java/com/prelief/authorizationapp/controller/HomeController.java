067c97e1221b513807aa97d0f31c3f4271fd4cfc
preliefp <preliefp@gmail.com>
2019-12-15 21:02:56 +0530

    demo

---
+++ spring_rest_demo-2/src/main/java/com/prelief/authorizationapp/controller/HomeController.java
@@ -0,0 +1,38 @@
+package com.prelief.authorizationapp.controller;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class HomeController {
+ private Map<String, LocalDateTime> usersLastAccess = new HashMap<>();
+ @GetMapping("/")
+ 
+ public String getCurrentUser(@AuthenticationPrincipal OidcUser user, Model model) {
+ 
+     String email = user.getEmail();
+ 
+ 
+    model.addAttribute("email", email);
+ 
+     model.addAttribute("lastAccess", usersLastAccess.get(email));
+ 
+     model.addAttribute("firstName", user.getGivenName());
+ 
+     model.addAttribute("lastName", user.getFamilyName());
+ 
+ 
+     usersLastAccess.put(email, LocalDateTime.now());
+ 
+ 
+     return "home";
+ 
+ }
+
+}
---
+++ spring_rest_demo-2/src/test/java/com/prelief/authorizationapp/SpringRestDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prelief.authorizationapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
